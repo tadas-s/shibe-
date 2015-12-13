36faee80736702ecc8174a4fada8ebd031047503
Tadas Sasnauskas <tadas@yoyo.lt>
2015-12-13 17:30:55 +0000

    Quick/dirty PostgreSQL install

--- provision.sh
+++ provision.sh
@@ -6,7 +6,7 @@ if [ ! -x "/usr/bin/ansible" ]; then
     echo "Will install Ansible..."
     apt-add-repository -y ppa:ansible/ansible
     apt-get update
-    apt-get install -y ansible
+    apt-get install -y ansible python-psycopg2
 fi
 
 ansible-playbook -i inventory site.yml --limit `hostname -f` --connection=local
